bfe829ff3311a6071fe6a3c6455e629800224df2
bugrice <manssik0820@gmail.com>
2024-07-23 00:50:38 +0900

    20240723

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <a>
-          Gemini API 프로젝트 / TagDoctor
+           Gemini API & TagDoctor
         </a>
       </header>
     </div>
