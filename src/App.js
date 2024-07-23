3dae6b5e40c1166ea77a388331008a87e7b070c6
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-07-24 01:39:35 +0900

    2024-07-24

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a>
-           Gemini API & TagDoctor test 21314
-        </a>
+        <div className="search-bar">
+          <input type="text" placeholder="해쉬태그 또는 직접 입력" />
+          <button className="search-button">
+            <img src="search_icon.png" alt="Search" />
+          </button>
+        </div>
+        <div className="categories">
+          <div className="category">머리</div>
+          <div className="category">가슴</div>
+          <div className="category">복부</div>
+          <div className="category">팔</div>
+          <div className="category">다리</div>
+        </div>
       </header>
     </div>
   );
