f68516490f35a51219411ff871265238f9853420
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-07-31 01:54:17 +0900

    2024-07-31

--- src/components/Page5.jsx
+++ src/components/Page5.jsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react'; // useState 추가
 import { useNavigate } from 'react-router-dom';
 import '../styles/Page5.css';
 import HomeLogo from '../imgs/Home_Logo.png';
 
 function Page5() {
   const navigate = useNavigate();
+  const [selectedPart, setSelectedPart] = useState('');
+
+  const handleNext = () => {
+    if (selectedPart) {
+      navigate('/page6');
+    }
+  };
 
   return (
-    <div>
-      <h1>Page 5</h1>
-      <button >?</button>
-      <button  onClick={() => navigate('/')}>Home</button>
-      <p>Welcome to Page 5!</p>
-      <button onClick={() => navigate('/page4')}>Previous</button>
-      <button onClick={() => navigate('/last')}>Next</button>
+    <div className="container">
+      <button className="info-button">?</button>
+      <button className="home-button" onClick={() => navigate('/')}>
+        <img src={HomeLogo} alt="Home" className="home-logo" />
+        Home
+      </button>
+      <div className='question'>
+        <h1>This is the last one!</h1>
+      </div>
+        
+      <div className="progress">
+        <div className="progress-step active">1</div>
+        <div className="progress-step active">2</div>
+        <div className="progress-step active">3</div>
+        <div className="progress-step active">4</div>
+        <div className="progress-step active">5</div>
+      </div>
+
+      <div className="input-box">
+        <p>Please write it down in detail.</p>
+      </div>
+
+      <div className="navigation-buttons">
+        <button className="nav-button" onClick={() => navigate('/page4')}>Previous</button>
+        <button className="nav-button next-button" onClick={handleNext}>Next</button>
+      </div>
+
+      <footer className="footer">
+        <h5>Tag-Doctor</h5>
+      </footer>
     </div>
   );
 }
--- src/components/Page6.jsx
+++ src/components/Page6.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function LastPage() {
+function Page6() {
   return (
     <div>
       <h1>Last Page</h1>
@@ -9,4 +9,4 @@ function LastPage() {
   );
 }
 
-export default LastPage;
+export default Page6;
