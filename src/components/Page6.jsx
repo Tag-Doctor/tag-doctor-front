ed9341314f669e360a674116fe4292e84d867b29
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-08-04 21:32:50 +0900

    2024-08-04

--- src/components/Page6.jsx
+++ src/components/Page6.jsx
@@ -10,40 +10,36 @@ function Page6() {
   const { selectedAge, selectedPart, selectedSubPart, selectedDetail, additionalInfo } = location.state || {}; // << 추가
   console.log(additionalInfo); // 확인용
   return (
-    <div className="page6-container">
-      <div className="header">
-        <img src={logo} alt="Logo" className="logo_3" />   
-        <button className="home-button" onClick={() => navigate('/')}>
-          <img src={HomeLogo} alt="Home" className="home-logo" />
-          Home
-        </button>
-      </div>
-      <div className="content">
-        <h1>Diagnostic results</h1>
-        <div className="results-box">
-          <p><strong>Age Group :  </strong> {selectedAge}</p>
-          <p><strong>Selected Part :  </strong> {selectedPart}</p>
-          <p><strong>Selected Sub-Part :  </strong> {selectedSubPart}</p>
-          <p><strong>Selected Detail :  </strong> {selectedDetail}</p>
-          
-        </div>
+      <div className="page6-container">
+          <div className="header">
+              <img src={logo} alt="Logo" className="logo_3"/>
+              <button className="home-button" onClick={() => navigate('/')}>
+                  <img src={HomeLogo} alt="Home" className="home-logo"/>
+                  Home
+              </button>
+          </div>
+          <div className="content">
+              <h1>Diagnostic results</h1>
+              <div className="results-box">
+                  <p><strong>Age Group : </strong> {selectedAge}</p>
+                  <p><strong>Selected Part : </strong> {selectedPart}</p>
+                  <p><strong>Selected Sub-Part : </strong> {selectedSubPart}</p>
+                  <p><strong>Selected Detail : </strong> {selectedDetail}</p>
+              </div>
+          </div>
 
-      </div>
+          <div className="result-box">
+              <ReactMarkdown rehypePlugins={[rehypeRaw]} className="result-textarea">{diagnosticResults}</ReactMarkdown>
+          </div>
 
-      <div className="result-box">
-        <textarea
-          className="result-textarea"
-          disabled></textarea>
-      </div>
+          <div className="navigation-buttons">
+              <button className="nav-button" onClick={() => navigate('/page1')}>Again</button>
+          </div>
 
-      <div className="navigation-buttons">
-      <button className="nav-button" onClick={() => navigate('/page1')}>Again</button>
+          <footer className="footer">
+              <h5>Tag-Doctor</h5>
+          </footer>
       </div>
-
-      <footer className="footer">
-        <h5>Tag-Doctor</h5>
-      </footer>
-    </div>
   );
 }
 
